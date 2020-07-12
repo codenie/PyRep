f80438364bf20a05ddf2361a80f402c48cf64e81
nanbaima <renanbaima@gmail.com>
2020-07-12 15:20:56 -0300

    NAO arm implementation
    NAO arm class renamed and restructured to a better implementation and control

--- pyrep/robots/arms/nao_arm.py
+++ pyrep/robots/arms/nao_arm.py
@@ -1,13 +1,13 @@
 from pyrep.robots.arms.arm import Arm
 
 
-class NAOLeft(Arm):
+class NAOLeftArm(Arm):
 
     def __init__(self, count: int = 0):
         super().__init__(count, 'NAO_leftArm', 5, base_name='NAO')
 
 
-class NAORight(Arm):
+class NAORightArm(Arm):
 
     def __init__(self, count: int = 0):
         super().__init__(count, 'NAO_rightArm', 5, base_name='NAO')
